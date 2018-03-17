9d8cc5ea7cec82a5a5c6441cadf891535c2d9569
Sanghyeok Park <pps0209@gmail.com>
2018-03-17 05:03:50 -0700

    recover traverse_process

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -5,6 +5,36 @@
 #include <asm-generic/uaccess.h>
 #include <linux/prinfo.h>
 
+void traverse_process(struct task_struct* task, struct trav_result* tvr) {
++	struct prinfo* pri;
++	struct task_struct* child;
++	struct task_struct* sibling;
++	int number = tvr->nr;
++	/* task_struct
++	   char comm[]
++	   volatile long state
++	   pid_t pid
++	   struct task_struct __rcu *real_parent
++	   struct list_head children
++	   struct list_head sibling
++	   task_struct->cred->uid
++	   */
++
++	/* from list.h
++	   list_entry(ptr, type, member)
++	   list_first_entry(ptr, type, member)
++	   list_for_each(post, head)
++	*/
++
++	//setting prinfo struct
++	*(pri+number).state = task->state;
++	*(pri+number).pid = task->pid;
++	*(pri+number).parent_pid = task->parent->pid;
++	*(pri+number).first_child_pid = list_first_entry();
++	*(pri+number).next_sibling_pid = list_first_entry();
++	*(pri+number).uid = task->cred->uid;
++	*(pri+number).comm = ;
+}
 
 asmlinkage int sys_ptree(struct prinfo* buf, int* _nr) {
     int nr;
