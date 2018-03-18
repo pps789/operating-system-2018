71e075d0af1bca5121a50c14c4f24db6de5b8a37
Ubuntu_16.04 <minimus@snu.ac.kr>
2018-03-18 05:52:38 -0700

    traverse_process alpha

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -5,41 +5,34 @@
 #include <asm-generic/uaccess.h>
 #include <linux/prinfo.h>
 
+struct trav_result {
+	struct prinfo *data;
+	int nr;
+	int nr_max;
+}
+
 void traverse_process(struct task_struct* task, struct trav_result* tvr) {
-+	struct prinfo* pri;
-+	struct task_struct* child;
-+	struct task_struct* sibling;
-+	int number = tvr->nr;
-+	/* task_struct
-+	   char comm[]
-+	   volatile long state
-+	   pid_t pid
-+	   struct task_struct __rcu *real_parent
-+	   struct list_head children
-+	   struct list_head sibling
-+	   task_struct->cred->uid
-+	   */
-+
-+	/* from list.h
-+	   list_entry(ptr, type, member)
-+	   list_first_entry(ptr, type, member)
-+	   list_for_each(post, head)
-+	*/
-+
-+	//setting prinfo struct
-+	*(pri+number).state = task->state;
-+	*(pri+number).pid = task->pid;
-+	*(pri+number).parent_pid = task->parent->pid;
-+	*(pri+number).first_child_pid = list_first_entry();
-+	*(pri+number).next_sibling_pid = list_first_entry();
-+	*(pri+number).uid = task->cred->uid;
-+	*(pri+number).comm = ;
+	
+	struct prinfo* buf;
+	//measure size
+	if(tvr->nr > tvr->nr_max)
+		return;
+	//record process down here
+	record_process(task, buf);
+	(tvr->nr)++;
+	//recursion and iteration start
+	struct list_head* child_list; 
+	list_for_each(child_list, &task->children) {
+		struct task_struct* child = list_entry(child_list, struct task_struct, sibling);
+		traverse_process(child, tvr);
+	}
 }
 
 asmlinkage int sys_ptree(struct prinfo* buf, int* _nr) {
     int nr;
-    
-    // check whether buff or nr is NULL
+    struct prinfo* data;
+    struct trav_result tvr;
+	// check whether buff or nr is NULL
     if(buf == NULL || _nr == NULL) return -EINVAL;
 
     // check accessibility of user memory
@@ -49,11 +42,16 @@ asmlinkage int sys_ptree(struct prinfo* buf, int* _nr) {
     if(copy_from_user(&nr, _nr, sizeof(int)) != 0) return -EFAULT;
     if(nr < 1) return -EINVAL;
     if(access_ok(VERIFY_WRITE, buf, sizeof(struct prinfo) * nr) == 0) return -EFAULT;
-	
+    //memory allocation
+	data = kmalloc(sizeof(struct prinfo)*nr, GFP_KERNEL);
+	//set trav_result
+	tvr.data = dat;
+	tvr.nr = 0;
+	tvr.nr_max = nr;
+
+	//ready to traverse
     read_lock(&tasklist_lock);
-    // do some jobs...
-    // traverse_process();
-    // traverse ends
+    traverse_process();
     read_unlock(&tasklist_lock);
 
     return 0;
