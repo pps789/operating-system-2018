15725ef2ae9d4b9edd0fb857b70b355f9d281cd5
rkd8923 <rkd8923@gmail.com>
2018-03-19 21:46:20 +0900

    test code with tap

--- test/test_ptree.c
+++ test/test_ptree.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
-
 #define __NR_ptree 380
 #define NR_MAX 300
 struct prinfo {
@@ -15,25 +14,42 @@ struct prinfo {
 	long uid;
 	char comm[64];
 };
-void print_ptree(struct prinfo *p, int nr) {
-	int i=0;
+void print_form(struct prinfo *p, int idx, int now_tap) {
+	int i;
+	for (i=0; i<now_tap; i++) printf("  ");
+	printf("%s,%d,%ld,%d,%d,%d,%d\n", 
+	p[idx].comm, p[idx].pid, p[idx].state,
+	p[idx].parent_pid, p[idx].first_child_pid,
+	p[idx].next_sibling_pid, p[idx].uid);
+}
+void print_ptree(struct prinfo *ptree, int nr) {
+	int now = 0, stack_pnt = 0;
+	int stack[nr];
+	int i;
 	for (i=0; i<nr; i++){
-		printf("%s,%d,%ld,%d,%d,%d,%d\n", 
-			p[i].comm, p[i].pid, p[i].state,
-			p[i].parent_pid, p[i].first_child_pid,
-			p[i].next_sibling_pid, p[i].uid);
+		
+		print_form(ptree, i, now);
+		
+		if (ptree[i].next_sibling_pid != 0) {
+			stack[stack_pnt] = now;	stack_pnt++;
+		}
+
+		if (ptree[i].first_child_pid != 0) {
+			now++;
+		}
+		else { 
+			stack_pnt--; now = stack[stack_pnt]; 
+		}
 	}
 }
 
 int main() {
 	int nr=NR_MAX;
-	
 	struct prinfo *buf = (struct prinfo*)malloc(sizeof(struct prinfo)*NR_MAX);
 	syscall(__NR_ptree, buf, &nr);
-	
-	printf("syscall test start!\n");
+	printf("---------------syscall test start!----------------\n");
 	print_ptree(buf, nr);
-	printf("syscall test end!\n");
+	printf("---------------syscall test end!----------------\n");
 	free(buf);
 	return 0;
 }
