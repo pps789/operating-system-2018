129955cdbedb9da24d649a584035b0fcbef43c1f
JSW <minimus@snu.ac.kr>
2018-04-06 01:43:00 -0700

    added availance check

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -9,15 +9,71 @@
 #include<linux/list.h>
 #include<linux/signal.h>
 
+#define TYPE_READ 1
+#define TYPE_WRITE 2
+
 static struct list_head pend_list = LIST_HEAD_INIT(pend_list);
 static struct list_head acq_list = LIST_HEAD_INIT(acq_list);
-static int[360] ref_counter = {0, };
+static int ref_counter[360] = {0, };
 
 struct rot_lock_t {
 	int degree;
 	int range;
+	int type;
 	pid_t pid;
+	struct list_head now; 
 };
+
+static int lock_available(struct rot_lock_t *p) {
+	int degree = p->degree;
+	int range = p->range;
+	pid_t pid = p->pid;
+	int type = p->type;
+	struct list_head now = p->now;
+
+	int lower = (degree - range)%360;
+	if(lower < 0)
+		lower = lower + 360;
+	int upper = (degree + range)%360;
+
+	int i = lower;
+	bool avail = true;
+	if(type == TYPE_WRITE) {
+		do {
+			avail = avail && (ref_counter[i] == 0);
+			i = (i++)%360;
+		}
+		while(i != upper);
+
+		if(avail == true) {
+			//list add to acq_list
+			list_add_tail(&now, &acq_list);
+			int j = lower;
+			do {
+				ref_counter[j] == -1;
+			}
+			while(j != upper);
+		}
+
+	}
+	else {
+		do {
+			avail = avail && (ref_counter[i] >= 0);
+			i = (i++)%360;
+		}
+		while (i != upper);
+		
+		if(avail == true) {
+			list_add_tail(&now, &acq_list);
+			int j = lower;
+			do {
+				ref_counter[j] == -1;
+			}
+			while (j != upper);
+		}
+	}
+	return;
+}
 
 int sys_set_rotation(int dgree) {
 }
