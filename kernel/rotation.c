332ebca85356327c2e6984b9a7cb94a4cc9df6f7
Sanghyeok Park <pps0209@gmail.com>
2018-04-07 08:25:52 -0700

    fix circular ranged loop

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -14,134 +14,150 @@
 
 static struct list_head pend_head = LIST_HEAD_INIT(pend_list);
 static struct list_head acq_head = LIST_HEAD_INIT(acq_list);
-//for refcounter, -1 means write lock is already locked at that area
-//0 means there is no lock at that area
-//positives means that there are number of read locks equal to the positive
+/* 
+ * for ref_counter, -1 means write lock is already locked at that area
+ * 0 means there is no lock at that area
+ * positives means that there are number of read locks equal to the positive
+ */
 static int ref_counter[360] = {0, };
 
 struct rot_lock_t {
-	int degree;
-	int range;
-	int type;
-	pid_t pid;
-	struct list_head loc; 
+    int degree;
+    int range;
+    int type;
+    pid_t pid;
+    struct list_head loc; 
 };
 
 DEFINE_SPINLOCK(rot_spinlock);
 
-//checking if two rot_lock_t are equal or not
+/* checking if two rot_lock_t are equal or not */
 static int rot_lock_t_equals(struct rot_lock_t *f, struct rot_lock_t *s) {
-	if(f->degree == s->degree) {
-		if(f->range == s->range) {
-			if(f->type == s->type) {
-				if(f->pid == s->pid) {
-					return 1;	//true
-				}
-			}
-		}
-	}
-	return 0;	//false
+    if(f->degree == s->degree) {
+        if(f->range == s->range) {
+            if(f->type == s->type) {
+                if(f->pid == s->pid) {
+                    return 1; // true
+                }
+            }
+        }
+    }
+    return 0; // false
 }
 
-//This function checks if new rot_loc_t can be inserted into acq_list
-//and insert it if available
+/*
+ * This function checks if new rot_loc_t can be inserted into acq_list
+ * and insert it if available
+ *
+ * If success, return 1.
+ * Oterwise, return 0.
+ * You should call this function with rot_spinlock
+ */
 static int lock_available(struct rot_lock_t *p) {
-	//variables
-	int degree = p->degree;
-	int range = p->range;
-	int type = p->type;
-	struct list_head loc = p->loc;
-	int lower = (degree - range)%360;
-	if(lower < 0)
-		lower = lower + 360;
-	int upper = (degree + range)%360;
-	int i, j = lower;
-	bool avail = true;
-	struct rot_lock_t *data;
-	
-	//check the type of p
-	if(type == TYPE_WRITE) {
-		do {
-			//if there is no lock in area
-			avail = avail && (ref_counter[i] == 0);
-			i = (i+1)%360;
-		}
-		while(i != upper);
+    // variables
+    int degree = p->degree;
+    int range = p->range;
+    int type = p->type;
+    struct list_head loc = p->loc;
 
-		if(avail == true) {
-			//list add to acq_list
-			data = kmalloc(sizeof(struct rot_lock_t), GFP_KERNEL);
-			data = &p;
-			list_add_tail(&loc, &acq_list);
-			do {
-				ref_counter[j] = -1;
-			}
-			while(j != upper);
-		}
+    // lower: 'beginning' degree (inclusive)
+    int lower = (degree - range)%360;
+    // upper: 'ending' degree (inclusive)
+    int upper = (degree + range)%360;
+    // return value
+    int avail = 1;
+    struct rot_lock_t *data;
 
-	}
-	else {
-		do {
-			avail = avail && (ref_counter[i] >= 0);
-			i = (i+1)%360;
-		}
-		while (i != upper);
-		
-		if(avail == true) {
-			list_add_tail(&loc, &acq_list);
-			do {
-				ref_counter[j] = -1;
-			}
-			while (j != upper);
-		}
-	}
-	return avail; // true: 1 false: 0
+    // TODO: We should investigate pending list...
+
+
+    if(type == TYPE_WRITE) {
+        int i;
+        for(i=0; i<=range*2; i++) {
+            // if type is WRITE, all value of ref_counter in range should be 0
+            if(ref_counter[(lower+i)%360] != 0){
+                avail = 0;
+            }
+        }
+
+        // TODO: remove this!
+        if(avail) {
+            //list add to acq_list
+            data = kmalloc(sizeof(struct rot_lock_t), GFP_KERNEL);
+            data = &p;
+            list_add_tail(&loc, &acq_list);
+            do {
+                ref_counter[j] = -1;
+            }
+            while(j != upper);
+        }
+
+    }
+    else {
+        int i;
+        for(i=0; i<=range*2; i++){
+            if(ref_counter[(lower+i)%360] >= 0){
+                avail = 0;
+            }
+        }
+
+        // TODO: remove this!
+        if(avail == true) {
+            list_add_tail(&loc, &acq_list);
+            do {
+                ref_counter[j] = -1;
+            }
+            while (j != upper);
+        }
+    }
+
+    return avail; // true: 1 false: 0
 }
 
 //this function checks if the same rot_lock_t exists in acq_list
 //if it exists, delete and return 1
 static int lock_exists(struct rot_lock_t *p) {
-	//variables
-	int degree = p->degree;
-	int range = p->range;
-	pid_t pid = p->pid
-	int type = p->type;
-	struct list_head loc = p->loc;
-	int lower = (p->degree - p->range)%360;
-	if(lower < 0)
-		lower = lower + 360;
-	int upper = (p->degree + p->range)%360;
-	struct list_head *cursor;
+    //variables
+    int degree = p->degree;
+    int range = p->range;
+    pid_t pid = p->pid
+        int type = p->type;
+    struct list_head loc = p->loc;
+    int lower = (p->degree - p->range)%360;
+    if(lower < 0)
+        lower = lower + 360;
+    int upper = (p->degree + p->range)%360;
+    struct list_head *cursor;
 
-	if(type == TYPE_WRITE) {
-		//soft filter
-		//checks degree, degree+range, degree-range has write lock or not
-		if((ref_counter[lower] == -1) && (ref_counter[upper] == -1) && (ref_counter[degree] == -1)) {
-			//iterate in reverse order
-			list_for_each_entry_reverse(cursor, &acq_head, loc) {
-				if(rot_lock_t_equal(p, cursor)) {
-					//take this node
-					list_del_entry(cursor);
-					//break and return
-					return 1; //success
-				}
-			}
-		}
-	}
-	else {
-		//soft filter
-		//checks degree, degree+range, degree-range have read lock
-		if((ref_counter[lower] >0) && (ref_counter[upper] >0) && (ref_counter[degree] > 0)) {
-			list_for_each_entry_reverse(cursor, &acq_head, loc) {
-				if(rot_lock_t_equals(p, cursor)) {
-					//take this node
-					list_del_entry(cursor);
-					return 1; //success
-				}
-			}
-		}
-	}
-	return 0; //fail - nothing happens
+    if(type == TYPE_WRITE) {
+        //soft filter
+        //checks degree, degree+range, degree-range has write lock or not
+        if((ref_counter[lower] == -1) && (ref_counter[upper] == -1) && (ref_counter[degree] == -1)) {
+            //iterate in reverse order
+            list_for_each_entry_reverse(cursor, &acq_head, loc) {
+                if(rot_lock_t_equal(p, cursor)) {
+                    //take this node
+                    list_del_entry(cursor);
+                    //break and return
+                    return 1; //success
+                }
+            }
+        }
+    }
+    else {
+        //soft filter
+        //checks degree, degree+range, degree-range have read lock
+        if((ref_counter[lower] >0) && (ref_counter[upper] >0) && (ref_counter[degree] > 0)) {
+            list_for_each_entry_reverse(cursor, &acq_head, loc) {
+                if(rot_lock_t_equals(p, cursor)) {
+                    //take this node
+                    list_del_entry(cursor);
+                    return 1; //success
+                }
+            }
+        }
+    }
+    return 0; //fail - nothing happens
 }
 
 
