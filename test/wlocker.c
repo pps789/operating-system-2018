a4f24ed313796163e9b7e3acaf038f97982c75be
Ubuntu_16.04 <minimus@snu.ac.kr>
2018-04-16 08:12:35 -0700

    test files

--- test/wlocker.c
+++ test/wlocker.c
@@ -6,7 +6,7 @@
 int main() {
 	int unlock = 1;
 	int d = 0;
-	for(d=0; d<360; d= d+60) {
+	for(d=0; d<360; d= d+90) {
 		unlock = syscall(382, d, 40);
 		printf("write locked at %d+40\n", d);
 	}
--- test/wunlocker.c
+++ test/wunlocker.c
@@ -6,7 +6,7 @@
 int main() {
 	int unlock = 1;
 	int d = 0;
-	for(d=0; d<360; d= d+60) {
+	for(d=0; d<360; d= d+90) {
 		unlock = syscall(385, d, 40);
 		printf("write unlocked at %d+40\n", d);
 	}
