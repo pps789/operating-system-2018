29db4a1cebd107e3edb93e587893b03b1c2742cf
Jiwon Kang <rkd8923@gmail.com>
2018-04-16 22:52:01 +0900

    test add error handling

--- test/selector.c
+++ test/selector.c
@@ -1,34 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <linux/rotation.h>
 #include <unistd.h>
 int main(int argc, char* argv[]) {
 	FILE *f;
-	int num = atoi(argv[1]);
+	int num;
 	int lock;
 	int unlock;
-	if (argv[0] == NULL) {
-    perror("need number\n");
+  int i; 
+	int check;
+	// check input
+	if (argc != 2) {
+    perror("input error");
+		return 1;
 	}
+	// check input is digit
+	for(i=0;i<strlen(argv[1]);i++) {
+    if (argv[1][i] < '0' || argv[1][i] > '9') {
+			perror("input is not digit");
+			return 1;
+		}
+	}
+	num = atoi(argv[1]);
+
 	while(1) {
+    // lock and check error
     lock = syscall(382, 90, 90);
     if(lock<0) {
-      perror("write lock error\n");
-      return 0;
+      perror("write lock error");
+      return 1;
+		}
+ 
+    // file open and check error
+		f = fopen("integer", "w");
+		if (f == NULL) {
+      perror("file open error");
+			return 1;
 		}
 
-		f = fopen("integer", "w");
-	  fprintf(f, "%d", num);
-		f = fopen("integer", "w");
 		fprintf(f, "%d", num);
 		printf("selector: %d\n", num);
-		fclose(f);
 		
+		// file close and check error
+		check = fclose(f);
+		if (check != 0) {
+      perror("file close error");
+			return 1;
+		}
+	
+		// unlock and check error
 		unlock = syscall(385, 90, 90);
 		if(unlock<0) {
-      perror("write unlock error\n");
-		  return 0;
+      perror("write unlock error");
+		  return 1;
 		}
 
 		num++;
--- test/trial.c
+++ test/trial.c
@@ -9,32 +9,60 @@ int main(int argc, char* argv[]) {
 	int unlock;
 	int num;
 	int i;
-	while(1) {
+	int check;
+	// check input
+	if (argc != 2) {
+    perror("input error");
+		return 1;
+	}
+
+	while(1) { 
+
+    // lock and check error
     lock = syscall(381, 90, 90);
 		if(lock<0) {
-      perror("read lock error\n");
-			return 0;
+      perror("read lock error");
+			return 1;
 		}
+    // file open and check error
 		f = fopen("integer", "r");
-		fscanf(f, "%d", &num);
-		fclose(f);
-		printf("trial-%s: %d = ", argv[1], num);
+		if (f == NULL) {
+      perror("file open error");
+			return 1;
+		}
 
-		for(i=2;i<=num;i++){
-			if(num%i==0) {
-				printf("%d", i);
-				num /= i;
-				if(num!=1) {
+		fscanf(f, "%d", &num);
+
+    // file close and check error
+		check = fclose(f);
+		if (check != 0) {
+      perror("file close error");
+			return 1;
+		}
+
+		printf("trial-%s: %d = ", argv[1], num);
+    if (num<2) {
+      printf("%d\n", num);
+		}
+    else {
+		  for(i=2;i<=num;i++){
+			  if(num%i==0) {
+				  printf("%d", i);
+				  num /= i;
+				  if(num!=1) {
 					printf(" * ");
-				}
-				i--;
-      }
+				  }
+				  i--;
+         }
+		   }
 		}
 	  printf("\n");
+
+		// unlock and check error
 	  unlock = syscall(383, 90, 90);
 	  if(unlock<0) {
       perror("read unlock error\n");
-		  return 0;
+		  return 1;
 	  }
 	}
 	return 0;
