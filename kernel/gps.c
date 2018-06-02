28d8baae47580068e0928de01f24535bb92b9193
Sanghyeok Park <pps0209@gmail.com>
2018-06-02 20:31:56 +0900

    implement set_gps_location syscall

--- kernel/gps.c
+++ kernel/gps.c
@@ -1,11 +1,38 @@
 #include <linux/gps.h>
 #include <linux/spinlock.h>
+#include <uapi/asm-generic/errno-base.h>
+#include <asm-generic/uaccess.h>
+#include <linux/unistd.h>
 
 DEFINE_SPINLOCK(gps_spinlock);
 struct gps_location curr_gps;
 
 int set_gps_location(struct gps_location __user *loc) {
-    // TODO: implement this!
+    struct gps_location gps;
+    if (loc == NULL) return -EINVAL;
+    if (access_ok(VERIFY_READ, loc, sizeof(struct gps_location)) == 0) return -EFAULT;
+    if (copy_from_user(&gps, loc, sizeof(struct gps_location)) != 0) return -EFAULT;
+
+    /* validate input */
+    if (gps.lat_integer < -90) return -EINVAL;
+    if (gps.lat_integer > 90) return -EINVAL;
+    if (gps.lng_integer < -180) return -EINVAL;
+    if (gps.lng_integer > 180) return -EINVAL;
+    if (gps.lat_fractional < 0) return -EINVAL;
+    if (gps.lat_fractional >= 1000000) return -EINVAL;
+    if (gps.lng_fractional < 0) return -EINVAL;
+    if (gps.lng_fractional >= 1000000) return -EINVAL;
+    if ((gps.lat_integer == -90 || gps.lat_integer == 90)
+            && (gps.lat_fractional > 0))
+        return -EINVAL;
+    if ((gps.lng_integer == -180 || gps.lng_integer == 180)
+            && (gps.lng_fractional > 0))
+        return -EINVAL;
+
+    spin_lock(&gps_spinlock);
+    curr_gps = gps;
+    spin_unlock(&gps_spinlock);
+
     return 0;
 }
 
