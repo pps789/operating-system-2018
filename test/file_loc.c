7a31798a96daa861022383c4a9cb4d836a58f5f9
JSW <minimus@snu.ac.kr>
2018-06-03 02:11:07 -0700

    user mode files

--- test/file_loc.c
+++ test/file_loc.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <asm-generic/errno.h>
-#include <linux/gps.h>
+
+struct gps_location {
+	int lat_integer;
+	int lat_fractional;
+	int lng_integer;
+	int lng_fractional;
+	int accuracy;
+};
 
 int main(int argc, char** argv) {
 	int ret;
 	char *path = argv[1];
 	printf("%s\n", path);
 
-	struct gps_location file_loc = malloc(sizeof(struct gps_location));
-	ret = syscall(381, path, file_loc);
+	struct gps_location* gps_loc = malloc(sizeof(struct gps_location));
+	ret = syscall(381, path, gps_loc);
 	if(ret != 0) {
 		return -1;
 	}
+
+	printf("https://www.google.co.kr/maps/place/");
+	printf("%d.%06d°N+", gps_loc->lat_integer, gps_loc->lat_fractional);
+	printf("%d.%06d°E\n", gps_loc->lng_integer, gps_loc->lng_fractional);
+	return 0;
 }
 
 
