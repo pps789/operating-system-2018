8072f9e8642abaf20b791bb86181012e91c0f296
JSW <minimus@snu.ac.kr>
2018-06-03 02:12:40 -0700

    user mode files

--- test/file_loc.c
+++ test/file_loc.c
@@ -15,14 +15,15 @@ struct gps_location {
 int main(int argc, char** argv) {
 	int ret;
 	char *path = argv[1];
-	printf("%s\n", path);
-
 	struct gps_location* gps_loc = malloc(sizeof(struct gps_location));
+	
 	ret = syscall(381, path, gps_loc);
+	//error case
 	if(ret != 0) {
 		return -1;
 	}
 
+	printf("path: %s\n", path);
 	printf("https://www.google.co.kr/maps/place/");
 	printf("%d.%06d°N+", gps_loc->lat_integer, gps_loc->lat_fractional);
 	printf("%d.%06d°E\n", gps_loc->lng_integer, gps_loc->lng_fractional);
